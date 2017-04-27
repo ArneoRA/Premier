8e93e3f4671295cbdbb4043371ce21d2a38ded0f
Radan <arneo.42@gmail.com>
2017-04-27 21:55:22 +0200

    Petite Modif sur ReadMe et PHP

--- Premiere_page.php
+++ Premiere_page.php
@@ -3,6 +3,6 @@
   <title>Test PHP</title>
  </head>
  <body>
- <?php echo '<p>Bonjour le monde</p>'; ?>
+ <?php echo '<p>Bonjour le monde et GitHub</p>'; ?>
  </body>
 </html>
--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 C'est mon premier Repository
 que je viens de modifier le 27/04/2017
 et maintenant je l'ai modifié depuis GitHub
+mais egalement depuis mon PC
